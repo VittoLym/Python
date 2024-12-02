71dfb4aa3a7192800dc9088dab4e632ffb64fbad
Vittossn <lexymdz@gmail.com>
2024-12-02 14:06:29 -0300

    fixing any bugs

--- extraerState.py
+++ extraerState.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,43 +22,45 @@ input_element_pswd = WebDriverWait(driver, 20).until(
 )
 input_element_pswd.send_keys("nodar2022+")
 
-input_element_pswd.submit()
-
+input_element_pswd.send_keys(Keys.RETURN)
 
 WebDriverWait(driver, 15).until(
-    EC.presence_of_all_elements_located((By.ID, 'portletComponentApplications_menuActionNormalModeApplications__id185_4_applicationLink'))
+    EC.presence_of_element_located((By.ID, 'portletComponentApplications_menuActionNormalModeApplications__id185_4_applicationLink'))
 )
 
-Input_consult = driver.find.element(By.ID, 'portletComponentApplications_menuActionNormalModeApplications__id185_4_applicationLink')
+Input_consult = driver.find_element(By.ID, 'portletComponentApplications_menuActionNormalModeApplications__id185_4_applicationLink')
 Input_consult.click()
 
-
-WebDriverWait(driver, 15).until(
-    EC.presence_of_all_elements_located((By.ID, 'label54_text'))
+driver.switch_to.frame("executionPanelApplications")
+input_tel = WebDriverWait(driver, 15).until(
+    EC.visibility_of_element_located((By.ID, 'att\\$nroLinea'))
 )
-
-input_tel = driver.find_element(By.ID, 'label54_text')
 input_tel.send_keys('3412003165')
 
+
 WebDriverWait(driver, 15).until(
-    EC.presence_of_element_located((By.ID, 'att$button6'))
+    EC.presence_of_element_located((By.ID, 'att\\$button6'))
 )
 
 input_search_consult = driver.find_element(By.ID, 'att$button6')
 input_search_consult.click()
 
 WebDriverWait(driver, 15).until(
-    EC.presence_of_all_elements_located((By.ID, 'array1_5_header'))
+    EC.presence_of_element_located((By.ID, 'array1_5_header'))
 )
 
-input_date = driver.find_element(By.ID, 'array1_5_header')
+input_date = driver.find_element(By.ID, 'grp\\$array1\\$detalle$0')
 input_date.click()
 
 WebDriverWait(driver, 15).until(
-    EC.presence_of_all_elements_located((By.ID, 'operador_comp'))
+    EC.presence_of_element_located((By.ID, 'operador_comp'))
 )
+number_opi = WebDriverWait(driver, 15).until(
+    EC.presence_of_element_located((By.ID, 'nroLineaCombo_comp'))
+)
+
 opi = driver.find_element(By.ID, 'operador_comp')
 
-print(f'{opi}')
+print(f'{opi.text}&&{number_opi.text}')
 
 time.sleep(5)
